62d5699be896596bc119f5ad3336f0b935a45791
MatheusPrM <73080153+MatheusPrM@users.noreply.github.com>
2023-06-06 15:10:57 -0300

    Testando o envio de email: Test Double manual

--- accounts/views.py
+++ accounts/views.py
@@ -9,12 +9,12 @@ from django.shortcuts import redirect
 
 def send_login_email(request):
     email = request.POST['email']
-    # send_mail(
-    #     'Your login link for Superlists',
-    #     'body text tbc',
-    #     'noreply@superlists',
-    #     [email],
-    # )
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email],
+    )
     return redirect('/')
 
 def login(request):
